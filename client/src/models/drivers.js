f992dea5018d87d28630ca630fadc7c8e575c970
Tam O'Donnell <thomashodonnell@gmail.com>
2019-06-07 17:33:35 +0100

    publishing drivers details and drivers names

--- client/src/models/drivers.js
+++ client/src/models/drivers.js
@@ -1,16 +1,26 @@
 const RequestHelper = require('../helpers/request_helper');
-const PubSub = require('../helpers/pub_sub.js')
+const PubSub = require('../helpers/pub_sub.js');
 
 const Drivers = function(){
     this.driversData = [];
+    this.driversNames = [];
 }
 
 Drivers.prototype.getData = function(){
     const request = new RequestHelper("http://ergast.com/api/f1/2019/drivers.json");
     request.get().then((data) => {
         this.driversData = data.MRData.DriverTable.Drivers
-
+        PubSub.publish('Drivers:drivers-ready', this.driversData)
+        this.publishDriversNames(this.driversData);
     })
+}
+
+Drivers.prototype.publishDriversNames = function(drivers){
+    drivers.forEach((driver) => {
+        fullName = driver.givenName + " " + driver.familyName;
+        this.driversNames.push(fullName)
+    })
+    PubSub.publish('Drivers:drivers-names-ready', this.driversNames)
 }
 
 module.exports = Drivers;

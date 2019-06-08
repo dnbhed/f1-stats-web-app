44eb45fc96619b69264b0eda71e5766ef9ac5bf7
Tam O'Donnell <thomashodonnell@gmail.com>
2019-06-08 14:46:17 +0100

    refactored to get driver ids

--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -3,28 +3,42 @@ const Highcharts = require('highcharts');
 
 const ChartView = function(container){
     this.container = container;
-    this.series = []
-    this.driver1Name = ''
-    this.driver2Name = ''
+    this.series = [];
+    this.driver1Details = {};
+    this.driver2Details = {};
 }
 
 ChartView.prototype.bindEvents = function(){
     
     PubSub.subscribe('Drivers:selected-driver-1-details', (event) => {
-        this.driver1Name = this.parseDriverDetails(event.detail[0])
-        this.renderChart(this.driver1Name, this.driver2Name)
+        this.parseDriver1Details(event.detail[0])
+        this.renderChart(this.driver1Details.driverName, this.driver2Details.driverName)
     })
     PubSub.subscribe('Drivers:selected-driver-2-details', (event) => {
-        this.driver2Name = this.parseDriverDetails(event.detail[0])
-        this.renderChart(this.driver1Name, this.driver2Name)
+        this.parseDriver2Details(event.detail[0])
+        this.renderChart(this.driver1Details.driverName, this.driver2Details.driverName)
     })
     
+    
+    PubSub.subscribe('Raceresults:race-results-ready', (event) => {
+        console.log(event.detail)
+    })
     
 }
 
-ChartView.prototype.parseDriverDetails = function(driver){
+ChartView.prototype.parseDriver1Details = function(driver){
     const driverName = driver.givenName + " " + driver.familyName;
-    return driverName;
+    const driverID = driver.diverId;
+    this.driver1Details.driverName = driverName;
+    this.driver1Details.driverId = driverID;
+    console.log(this.driver1Details)
+}
+
+ChartView.prototype.parseDriver2Details = function(driver){
+    const driverName = driver.givenName + " " + driver.familyName;
+    const driverID = driver.diverId;
+    this.driver2Details.driverName = driverName;
+    this.driver2Details.driverId = driverID;
 }
 
 ChartView.prototype.renderChart = function(driver1Name, driver2Name){

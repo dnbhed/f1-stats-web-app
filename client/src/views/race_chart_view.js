761dbf10a724c04831ca57583d1b47964ccfd49b
Tam O'Donnell <thomashodonnell@gmail.com>
2019-06-10 07:50:57 +0100

    refactor charts code

--- client/src/views/race_chart_view.js
+++ client/src/views/race_chart_view.js
@@ -62,8 +62,9 @@ RaceChartView.prototype.renderChart = function(results){
     const driver1Race = results[1];
     const driver2Qualy = results[2];
     const driver2Race = results[3];
+    console.log(driver1Qualy)
 
-    const driver1Code = driver1Qualy.code;
+    const driver1Code = driver1Qualy.driverCode;
     const driver1Q1 = parseFloat(driver1Qualy.q1);
     const driver1Q2 = parseFloat(driver1Qualy.q2);
     const driver1Q3 = parseFloat(driver1Qualy.q3);
@@ -71,7 +72,7 @@ RaceChartView.prototype.renderChart = function(results){
     const driver1Finish = parseInt(driver1Race.finishingPosition);
     const driver1Points = parseInt(driver1Race.points);
 
-    const driver2Code = driver2Qualy.code;
+    const driver2Code = driver2Qualy.driverCode;
     const driver2Q1 = parseFloat(driver2Qualy.q1);
     const driver2Q2 = parseFloat(driver2Qualy.q2);
     const driver2Q3 = parseFloat(driver2Qualy.q3);
@@ -79,35 +80,161 @@ RaceChartView.prototype.renderChart = function(results){
     const driver2Finish = parseInt(driver2Race.finishingPosition);
     const driver2Points = parseInt(driver2Race.points);
 
-    var myChart = Highcharts.chart(container, {    
+    // var myChart = Highcharts.chart(container, {    
 
+    //     chart: {
+    //         yAxis: {
+    //             min: 0,
+    //             max: 20,
+    //             minRange:20
+    //         }
+    //     },
+    //     title: {
+    //         text: 'Qualy and Race Results'
+    //     },
+    //     xAxis: {
+    //         categories: ['Q1', 'Q2', 'Q3','Grid', 'Finished', 'Points']
+    //     },
+    //     yAxis: {
+    //         title: {
+    //             text: 'Position'
+    //         }
+    //     },
+    //     series: [{
+    //         type: "column",
+    //         name: driver1Code,
+    //         data: [driver1Q1, driver1Q2, driver1Q3, driver1Grid, driver1Finish, driver1Points]
+    //     }, {
+    //         type: "column",
+    //         name: driver2Code,
+    //         data: [driver2Q1, driver2Q2, driver2Q3, driver2Grid, driver2Finish, driver2Points]
+    //     }]
+    // });
+
+
+    Highcharts.chart(container, {
         chart: {
-            type: 'column',
-            yAxis: {
-                min: 0,
-                max: 20,
-                minRange:20
-            }
+            zoomType: 'xy'
         },
         title: {
-            text: 'Qualy and Race Results'
+            text: `${driver1Code} vs ${driver2Code}`,
+            align: 'center'
         },
-        xAxis: {
-            categories: ['Q1', 'Q2', 'Q3','Grid', 'Finished', 'Points']
+        subtitle: {
+            text: 'Source: ergast.com',
+            align: 'center'
         },
-        yAxis: {
+        xAxis: [{
+            categories: ['Q1', 'Q2', 'Q3', 'Grid', 'Finished', 'Points'],
+            crosshair: true
+        }],
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: 'time',
+                style: {
+                    color: Highcharts.getOptions().colors[2]
+                }
+            },
             title: {
-                text: 'Position'
+                text: 'Time(s)',
+                style: {
+                    color: Highcharts.getOptions().colors[2]
+                }
+            },
+            opposite: true
+
+        }, { // Secondary yAxis
+            gridLineWidth: 0,
+            title: {
+                text: 'Position',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+            labels: {
+                format: 'P',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
             }
+
+        }, { // Tertiary yAxis
+            gridLineWidth: 0,
+            title: {
+                text: 'Points',
+                style: {
+                    color: Highcharts.getOptions().colors[1]
+                }
+            },
+            labels: {
+                format: '{value} Points',
+                style: {
+                    color: Highcharts.getOptions().colors[1]
+                }
+            },
+            opposite: true
+        }],
+        tooltip: {
+            shared: true
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            x: 80,
+            verticalAlign: 'top',
+            y: 55,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || 'rgba(255,255,255,0.25)'
         },
         series: [{
-            name: driver1Code,
-            data: [driver1Q1, driver1Q2, driver1Q3, driver1Grid, driver1Finish, driver1Points]
+            name: 'time',
+            type: 'column',
+            yAxis: 1,
+            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            tooltip: {
+                valueSuffix: ' '
+            }
+
         }, {
-            name: driver2Code,
-            data: [driver2Q1, driver2Q2, driver2Q3, driver2Grid, driver2Finish, driver2Points]
-        }]
+            name: 'Position',
+            type: 'column',
+            yAxis: 2,
+            data: [1016, 1016, 1015.9, 1015.5, 1012.3, 1009.5, 1009.6, 1010.2, 1013.1, 1016.9, 1018.2, 1016.7],
+            marker: {
+                enabled: false
+            },
+            dashStyle: 'shortdot',
+            tooltip: {
+                valuePrefix: 'P '
+            }
+
+        }, {
+            name: 'Points',
+            type: 'column',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+            tooltip: {
+                valueSuffix: ' points'
+            }
+        }],
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        floating: false,
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom',
+                        x: 0,
+                        y: 0
+                    }
+                }
+            }]
+        }
     });
+
 }
 
 

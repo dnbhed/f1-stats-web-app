3294650d1a0b1b2d2350d7f32e796da75d011ed2
Tam O'Donnell <thomashodonnell@gmail.com>
2019-06-28 11:16:11 +0100

    change package.son and add dev server

---
+++ server/devserver.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const parser = require('body-parser');
+
+
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+app.use(parser.json());
+
+app.listen(3001, function () {
+    console.log(`Listening on port ${this.address().port}`);
+});
